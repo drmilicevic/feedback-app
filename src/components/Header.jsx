de9f97c280e5515de505d47b48a9ef1aedac9ebc
Dragan Milicevic <dmilicevic@bettercollective.com>
2024-01-22 21:40:32 +0100

    Feedback app - CSS styling and default props values

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,30 @@
 import PropTypes from 'prop-types'
 
-function Header({ text }) {
-  return (
-    <header>
-      <div className="container">
-        <h2>{text}</h2>
-      </div>
-    </header>
-  )
+function Header({ text, bgColor, textColor }) {
+    const headerStyle = {
+        backgroundColor: bgColor, 
+        color: textColor
+    }
+
+    return (
+        <header style={headerStyle}>
+        <div className="container">
+            <h2>{text}</h2>
+        </div>
+        </header>
+    )
 }
 
 Header.defaultProps = {
-    text: 'Feedback UI default value',
+    text: 'Feedback UI App Example',
+    bgColor: 'rgba(0,0,0,0.4)',
+    textColor: '#ff659a'
 }
 
 Header.propTypes = {
     text: PropTypes.string,
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string
 }
 
 export default Header
