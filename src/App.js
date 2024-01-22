5b19291149dc90290a1aacd2c7ad403fc8ccfeea
Dragan Milicevic <dmilicevic@bettercollective.com>
2024-01-22 21:22:51 +0100

    Feedback app - header component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h1>My App</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
